0397225891c3880722e2430ac907cb05e15ff617
shirgnapp <gnapp3@gmail.com>
2024-11-24 21:16:07 +0200

    done

--- Algebra.java
+++ Algebra.java
@@ -13,7 +13,7 @@ public class Algebra {
    		System.out.println(minus(2,7));  // 2 - 7
 		System.out.println(minus(-2,7));  // 2 - 7
 		System.out.println(minus(-2,-7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+ 		System.out.println(times(4,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
@@ -37,6 +37,7 @@ public class Algebra {
 	else {
 		while (i < x2) {
 			x1++;
+			i++;
 		}
 	}
 	return x1;
@@ -48,7 +49,6 @@ public class Algebra {
 				x1++;
 				i++;
 			}
-			
 		}
 		else {
 		while (i > 0) {
@@ -58,47 +58,36 @@ public class Algebra {
 	}
 		return x1;
 	}
-
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		if (x1 > 0 && x2 < 0){
-			int counter=x2;
-			for(int i= 0; i > x1; i--) {
-				x2=minus(x2, counter);
-			} 
-			return x2;
-		}
-		if (x1 < 0 && x2 > 0) {
-			int counter=x1;
-			for(int i= 0; i > x2; i--) {
-				x1=minus(x1, counter);
-			} 
-			return x1;
-		}
-		if (x1 > 0 && x2 > 0) {
-			int counter = x1;
-			for(int i=0; i< x2; i++ ){
-				x1 = plus(x1, counter);
-			}
-			return x1;
-		}
-		if (x1 < 0 && x2 < 0) {
-			int PositivNumber1 = 0, PositivNumber2 = 0;
-			for (int j=0; j > x1; j--) {
+		int PositivNumber1 = x1;
+		int PositivNumber2 = x2;
+		if (x1 < 0) {
+			PositivNumber1 = 0;
+			for(int i=0; i>x1; i--){
 				PositivNumber1++;
-			} 
-			for (int n=0; n > x2; n--) {
-				PositivNumber2++;
-			} 
-			int counter = PositivNumber1;
-			for(int s=0; s< PositivNumber2; s++ ){
-				PositivNumber1 = plus(PositivNumber1, counter);
 			}
-			return PositivNumber1;
 		}
-		return 0;	
+		if (x2 < 0) {
+			PositivNumber2 = 0;
+			for(int j=0; j>x2; j--){
+				PositivNumber2++;
+			}	
 		}
-	
+		int i = 1, counter = PositivNumber1;
+		while (i < PositivNumber2) {
+			PositivNumber1 = plus(PositivNumber1, counter);
+			i++;
+		}
+			if (x1 < 0 && x2 > 0 || x1 > 0 && x2 < 0) {
+			int NegativeNumber = 0;
+			for(int n=0; n < PositivNumber1; n++) {
+				NegativeNumber--;
+			}
+			return NegativeNumber;
+		}
+	return PositivNumber1;
+}
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
 		int i=x;
@@ -134,59 +123,55 @@ public class Algebra {
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int PositivNumber1 = 0, PositivNumber2 = 0;
-		int counter = 1;
-		if (x1 == 0 || x2 > x1) {
+		if (x1 == 0 ) {
 			return 0;
 		}
-		if (x1 < 0 || x2 < 0) {
-			for(int n=0; n > x1; n--){
+		int PositivNumber1 = x1;
+		int PositivNumber2 = x2;
+		if (x1 < 0) {
+			PositivNumber1 = 0;
+			for(int i=0; i>x1; i--){
 				PositivNumber1++;
 			}
-			for(int j=0; j > x2; j--){
-			PositivNumber2++;
+		}
+		if (x2 < 0) {
+			PositivNumber2 = 0;
+			for(int j=0; j>x2; j--){
+				PositivNumber2++;
+			}	
+		}
+		if (x2 > x1) {
+			return 0;
+		}
+		int counter = 1;
+		while (x1 > x2) {
+			x1=minus(x1, x2);
+			if (x1 >= x2) {
+				counter ++;
 			}
-			int i = PositivNumber1;
-			while (PositivNumber1 > PositivNumber2) {
-				PositivNumber1 = minus(PositivNumber1, i);
-				if (PositivNumber1 >= i) {
-					counter++;
-				}
 		}
+		if (x1 > 0 && x2 < 0 || x1 < 0 && x2 > 0) {
+			int NegativeCounter = -1;
+			for(int j=0; j<counter; j++){
+				NegativeCounter--;
+			}
+			return NegativeCounter;
+		}
+		return counter;
 	}
-		else {
-			int i = x1;
-			while (x1 > x2) {
-				x1 = minus(x1, i);
-				if (x1 >= i) {
-					counter++;
-				}
-		}
-		if ((x2 < 0 && x1 > 0) || (x2 > 0  && x1 < 0)) {
-			counter = times(counter, -1);
-		}
-		if (x1 == x2) {
-			return counter;
-		}
-	}
-	return counter;
-	}
-
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
 		if (x1 == 0) {
 			return 0;
 		}
-		if (x2 > x1) {
-			return (minus(x2, x1));
+		int div = div(x1, x2);
+		div = times(div, x2);
+		int result = x1 - div;
+		if (result == x2) {
+			return 0;
 		}
 		else
-		{
-			int div = div(x1, x2);
-			int mod = times(x2, div); 
-			mod = minus(x1, mod);
-			return mod;
-		}
+		return result;
 	}
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
